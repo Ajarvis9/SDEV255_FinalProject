d2fd44b2de5c035c186200727db73eef6dd5c243
Amy Jarvis <ajarvis9@ivytech.edu>
2025-04-20 19:26:37 -0500

    changed next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/SDEV255_FinalProject/' : '',
+  basePath: isProd ? '/SDEV255_FinalProject' : '',
+  output: 'export'
+};
 
 export default nextConfig;
---
+++ src/app/coursesList/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return(
+    <main>
+        <h1>Courses List</h1>
+    </main>
+  );  
+}
